f28d0639e6745df55a72946cc68d2d05b9b34a9f
marye-99 <mesthersotoluque@gmail.com>
2025-09-29 00:02:27 -0500

    sidebarrr

---
+++ app/src/main/java/com/marcos/postresapp/presentation/ui/activity/SideActivity.kt
@@ -0,0 +1,50 @@
+package com.marcos.postresapp.presentation.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.marcos.postresapp.R
+
+class SideActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_side)
+
+        window.statusBarColor = getColor(R.color.morado)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(drawerLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // ActionBarDrawerToggle normal
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_nav,
+            R.string.close_nav
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Cambiar color del icono hamburguesa a blanco
+        toggle.drawerArrowDrawable.color = resources.getColor(android.R.color.white)
+    }
+}
