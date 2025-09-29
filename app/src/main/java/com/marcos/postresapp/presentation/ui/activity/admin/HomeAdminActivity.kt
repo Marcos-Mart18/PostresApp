26395759a72a196607236510909290db4f7609c3
Noelia-Rivera <noelia.rivera@upeu.edu.pe>
2025-09-28 22:41:06 -0500

    agregar_producto

--- app/src/main/java/com/marcos/postresapp/presentation/ui/activity/admin/HomeAdminActivity.kt
+++ app/src/main/java/com/marcos/postresapp/presentation/ui/activity/admin/HomeAdminActivity.kt
@@ -1,6 +1,8 @@
 package com.marcos.postresapp.presentation.ui.activity.admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +10,25 @@ import androidx.core.view.WindowInsetsCompat
 import com.marcos.postresapp.R
 
 class HomeAdminActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_admin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val cardAgregar     = findViewById<View>(R.id.cardAgregar)
+        val panelForm       = findViewById<View>(R.id.panelForm)
+        val rvProductos     = findViewById<View>(R.id.rvProductos)
+        val btnCrear        = findViewById<View>(R.id.btnCrear)
+
+        cardAgregar.setOnClickListener {
+            rvProductos.visibility = View.GONE
+            panelForm.visibility   = View.VISIBLE
         }
-    }
+
+        btnCrear.setOnClickListener {
+            panelForm.visibility   = View.GONE
+            rvProductos.visibility = View.VISIBLE
+        }
+
+        }
 }
